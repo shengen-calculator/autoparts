7cce21bf8ac44d4e0a6bb9441280212571512896
Vasyl Suddya <Vasiliy.Suddya@eu.jll.com>
2020-03-03 13:11:18 +0200

    add icons (Guaranted term, Good quality)

--- src/components/manager/search/SearchTableRow.js
+++ src/components/manager/search/SearchTableRow.js
@@ -2,6 +2,10 @@ import TableRow from "@material-ui/core/TableRow";
 import TableCell from "@material-ui/core/TableCell";
 import InfoIcon from "@material-ui/icons/Info";
 import React from "react";
+import Grid from '@material-ui/core/Grid';
+import ThumbUpIcon from '@material-ui/icons/ThumbUp';
+import CheckCircleOutlineIcon from '@material-ui/icons/CheckCircleOutline';
+import Tooltip from "@material-ui/core/Tooltip";
 
 export default function SearchTableRow(row, index, isSelected, handleClick) {
     const isItemSelected = isSelected(row.name);
@@ -17,16 +21,43 @@ export default function SearchTableRow(row, index, isSelected, handleClick) {
             key={row.id}
             selected={isItemSelected}
         >
-
             <TableCell padding="default" component="th" id={labelId} scope="row">
                 {row.brand}
             </TableCell>
-            <TableCell align="left">{row.number}</TableCell>
+            <TableCell align="left">
+                {
+                    row.isGoodQuality ?
+                    <Grid container>
+                        <Grid item>
+                            {row.number}
+                        </Grid>
+                        <Grid item>
+                            <Tooltip title="Відмінна якість">
+                                <ThumbUpIcon style={{fontSize: 10, marginBottom: 5, marginLeft: 3}}/>
+                            </Tooltip>
+                        </Grid>
+                    </Grid> : row.number
+                }
+            </TableCell>
             <TableCell align="left">{row.description}</TableCell>
             <TableCell align="right">{row.retail}</TableCell>
             <TableCell align="right">{row.cost}</TableCell>
             <TableCell align="right">{row.order}</TableCell>
-            <TableCell align="left">{row.term}</TableCell>
+            <TableCell align="left">
+                {
+                    row.isGuaranteedTerm ?
+                        <Grid container>
+                            <Grid item>
+                                {row.term}
+                            </Grid>
+                            <Grid item>
+                                <Tooltip title="Гарантований строк">
+                                    <CheckCircleOutlineIcon style={{fontSize: 12, marginBottom: 5, marginLeft: 3}}/>
+                                </Tooltip>
+                            </Grid>
+                        </Grid> : row.term
+                }
+            </TableCell>
             <TableCell align="left">{row.date}</TableCell>
             <TableCell align="center">
                 <InfoIcon/>
--- src/components/manager/search/VendorTable.js
+++ src/components/manager/search/VendorTable.js
@@ -4,24 +4,24 @@ import SearchTableRow from './SearchTableRow';
 import {TitleIconEnum} from "../../../util/Enums";
 
 
-function createData(id, brand, number, description, retail, cost, available, reserve, order, term, date) {
-    return { id, brand, number, description, retail, cost, available, reserve, order, term, date };
+function createData(id, brand, number, description, retail, cost, available, reserve, order, term, date, isGoodQuality, isGuaranteedTerm) {
+    return { id, brand, number, description, retail, cost, available, reserve, order, term, date, isGoodQuality, isGuaranteedTerm };
 }
 
 const rows = [
-    createData(1,'RUVILE', '5413', 'Свеча зажигания 3330', 64.53, 41.855,1,1,0,'1дн', '20.02.2020'),
-    createData(2,'RUVILE', '5413', '(16:00, Сб. до 13:00) BERU 14FR-5DU Свеча зажигания ULTRA', 51.30, 34.95, 5,1,0,'1дн', '20.02.2020' ),
-    createData(3,'RUVILE', '5413', '(16:00, Сб. до 13:00) BERU 14FR-5DU Свеча зажигания ULTRA', 24.40, 26.10, 5,1,0,'1дн', '20.02.2020'),
-    createData(4,'RUVILE', '5413', '(Вт/Чт 17:00, Сб. до 13:00) SPARK PLUG (14 FR-5 DU EA 0,8)', 24.99, 34.10, 5,1,0,'1дн', '20.02.2020'),
-    createData(5,'RUVILE', '5413', '(Вт/Чт 17:00, Сб. до 13:00) SPARK PLUG (14 FR-5 DU EA 0,8)', 49.44, 43.29, 5,1,0,'1дн', '20.02.2020'),
-    createData(6,'RUVILE', '5413', '(13:00, Сб. до 11:00) ŚWIECA ZAPŁONOWA PSA/FIAT', 87.90, 46.50, 5,1,0,'1дн', '20.02.2020'),
-    createData(7,'RUVILE', '5413', '(13:00, Сб. до 11:00) ŚWIECA ZAPŁONOWA PSA/FIAT', 37.90, 44.33, 5,1,0,'1дн', '20.02.2020'),
-    createData(8,'NGK', 'BCPR7ES', '(13:00, Сб. до 11:00) ŚWIECA ZAPŁONOWA PSA/FIAT', 96.4, 60.10, 5,1,0,'1дн', '20.02.2020'),
-    createData(9,'BOSCH', '0242245536', '(Вт/Чт до 17:30, Сб. до 12:00) Свеча зажигания FR7DCE 0.8', 65.23, 74.10, 5,1,0,'1дн', '20.02.2020'),
-    createData(10,'BOSCH', '0242245536', '(Вт/Чт до 17:30, Сб. до 12:00) Свеча зажигания FR7DCE 0.8', 98.11, 50.10, 5,1,0,'1дн', '20.02.2020'),
-    createData(11,'BOSCH', '0242245536', '(Вт/Чт до 17:30, Сб. до 12:00) Свеча зажигания FR7DCE 0.8', 81.21, 22.10, 5,1,0,'1дн', '20.02.2020'),
-    createData(12,'BOSCH', '0242245536', '(Вт/Чт до 17:30, Сб. до 12:00) Свеча зажигания FR7DCE 0.8', 99.89, 37.60, 5,1,0,'1дн', '20.02.2020'),
-    createData(13,'BOSCH', '0242245536', '(Вт/Чт до 17:30, Сб. до 12:00) Свеча зажигания FR7DCE 0.8', 63.01, 54.60, 5,1,0,'1дн', '20.02.2020'),
+    createData(1,'RUVILE', '5413', 'Свеча зажигания 3330', 64.53, 41.855,1,1,0,'1дн', '20.02.2020', true, true),
+    createData(2,'RUVILE', '5413', '(16:00, Сб. до 13:00) BERU 14FR-5DU Свеча зажигания ULTRA', 51.30, 34.95, 5,1,0,'1дн', '20.02.2020', true, false ),
+    createData(3,'RUVILE', '5413', '(16:00, Сб. до 13:00) BERU 14FR-5DU Свеча зажигания ULTRA', 24.40, 26.10, 5,1,0,'1дн', '20.02.2020', false, true),
+    createData(4,'RUVILE', '5413', '(Вт/Чт 17:00, Сб. до 13:00) SPARK PLUG (14 FR-5 DU EA 0,8)', 24.99, 34.10, 5,1,0,'1дн', '20.02.2020', false, true),
+    createData(5,'RUVILE', '5413', '(Вт/Чт 17:00, Сб. до 13:00) SPARK PLUG (14 FR-5 DU EA 0,8)', 49.44, 43.29, 5,1,0,'1дн', '20.02.2020', false, true),
+    createData(6,'RUVILE', '5413', '(13:00, Сб. до 11:00) ŚWIECA ZAPŁONOWA PSA/FIAT', 87.90, 46.50, 5,1,0,'1дн', '20.02.2020', false, false),
+    createData(7,'RUVILE', '5413', '(13:00, Сб. до 11:00) ŚWIECA ZAPŁONOWA PSA/FIAT', 37.90, 44.33, 5,1,0,'1дн', '20.02.2020', false, false),
+    createData(8,'NGK', 'BCPR7ES', '(13:00, Сб. до 11:00) ŚWIECA ZAPŁONOWA PSA/FIAT', 96.4, 60.10, 5,1,0,'1дн', '20.02.2020', false, false),
+    createData(9,'BOSCH', '0242245536', '(Вт/Чт до 17:30, Сб. до 12:00) Свеча зажигания FR7DCE 0.8', 65.23, 74.10, 5,1,0,'1дн', '20.02.2020', true, false),
+    createData(10,'BOSCH', '0242245536', '(Вт/Чт до 17:30, Сб. до 12:00) Свеча зажигания FR7DCE 0.8', 98.11, 50.10, 5,1,0,'1дн', '20.02.2020', false, false),
+    createData(11,'BOSCH', '0242245536', '(Вт/Чт до 17:30, Сб. до 12:00) Свеча зажигания FR7DCE 0.8', 81.21, 22.10, 5,1,0,'1дн', '20.02.2020', false, false),
+    createData(12,'BOSCH', '0242245536', '(Вт/Чт до 17:30, Сб. до 12:00) Свеча зажигания FR7DCE 0.8', 99.89, 37.60, 5,1,0,'1дн', '20.02.2020', false, false),
+    createData(13,'BOSCH', '0242245536', '(Вт/Чт до 17:30, Сб. до 12:00) Свеча зажигания FR7DCE 0.8', 63.01, 54.60, 5,1,0,'1дн', '20.02.2020', false, false),
 ];
 
 const headCells = [
