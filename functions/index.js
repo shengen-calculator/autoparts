3f2bb171d5a0dfc9198c70b293b667f3f7a767e8
Vasyl Suddya <Vasiliy.Suddya@eu.jll.com>
2020-03-14 22:33:20 +0200

    fix on create trigger

--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,9 @@
 const functions = require('firebase-functions');
 const admin = require('firebase-admin');
-const processSignUp = require('functions/processSignUp');
+const processSignUp = require('./processSignUp');
 
 admin.initializeApp();
 
-exports.sendWelcomeEmail = functions.auth.user().onCreate((user) => {
+exports.processSignUp = functions.auth.user().onCreate((user) => {
     return processSignUp(user);
 });
