dbeb3390ea8f4110bc4737be2f95309cc812b44d
Vasyl Suddya <Vasiliy.Suddya@eu.jll.com>
2020-03-19 14:16:11 +0200

    ajax in progress line fix

--- src/sagas/clientSaga.js
+++ src/sagas/clientSaga.js
@@ -1,24 +1,25 @@
 import {call, put} from 'redux-saga/effects';
 import * as types from '../redux/actions/actionTypes';
 import FunctionsApi from '../api/clientFunctions';
-import {beginApiCall} from "../redux/actions/apiStatusActions";
 
 export function* getClient(action) {
     try {
-        yield call(beginApiCall);
+        yield put({type: types.BEGIN_API_CALL});
         const data = yield call(FunctionsApi.getClientByVip, action.vip);
         yield put({type: types.LOAD_CLIENT_SUCCESS, client: data.data});
     } catch (e) {
+        yield put({type: types.API_CALL_ERROR});
         yield put({type: types.LOAD_CLIENT_FAILURE, text: e.message});
     }
 }
 
 export function* getPayments(action) {
     try {
-        yield call(beginApiCall);
+        yield put({type: types.BEGIN_API_CALL});
         const data = yield call(FunctionsApi.getPaymentsByVip, action.vip);
         yield put({type: types.LOAD_PAYMENTS_SUCCESS, payments: data.data});
     } catch (e) {
+        yield put({type: types.API_CALL_ERROR});
         yield put({type: types.LOAD_PAYMENTS_FAILURE, text: e.message});
     }
 }
