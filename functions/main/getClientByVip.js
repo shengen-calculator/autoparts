bf860a133f59a68029a2dee00350c65de1dd617d
Vasyl Suddya <vasya.suddya@gmail.com>
2020-04-17 12:59:51 +0300

    add info to the function if client use euro as currency

--- functions/main/getClientByVip.js
+++ functions/main/getClientByVip.js
@@ -10,16 +10,16 @@ const getClientByVip = async (data, context) => {
             'The function must be called with one argument "vip"');
     }
 
-    function createData(vip, fullName) {
-        return { vip, fullName };
+    function createData(vip, fullName, isEuroClient) {
+        return { vip, fullName, isEuroClient };
     }
 
     const rows = [
-        createData('1000','Юрий Петров'),
-        createData('2000','Сергей Иванов'),
-        createData('3000','Иван Сидоров'),
-        createData('4000','Николай Пупкин'),
-        createData('5000','Петр Комаровский'),
+        createData('1000','Юрий Петров', true),
+        createData('2000','Сергей Иванов', true),
+        createData('3000','Иван Сидоров', true),
+        createData('4000','Николай Пупкин', false),
+        createData('5000','Петр Комаровский', false),
     ];
 
     return rows.find(x => x.vip === data);
