90fecd703a7d6eafbf97d630d91b07a07a4b823d
Vasyl Suddya <vasya.suddya@gmail.com>
2020-04-19 07:37:47 +0300

    update create order, reserve functions, they should return created row

--- functions/search/createReserve.js
+++ functions/search/createReserve.js
@@ -4,11 +4,15 @@ const util = require('../util');
 const createReserve = async (data, context) => {
 
     util.CheckForManagerRole(context);
+    function createData(id, brand, number, description, note, quantity, vendor, euro, uah, orderDate, date, source) {
+        return {id, brand, number, description, quantity, note, vendor, euro, uah, orderDate, date, source };
+    }
 
     if (!data || !data.productId || typeof data.quantity === 'undefined' || !data.price || !data.vip ) {
         throw new functions.https.HttpsError('invalid-argument',
             'The function must be called with the next arguments "ProductId, Quantity, Price, Vip"');
     }
+    return createData(35, 'DELPHI', 'TA2913', 'Только что зарезервированая ŚWIECA ZAPŁONOWA PSA/FIAT', '', 2, '', 33.95, 872.52, '19.02.2020', '23.02.2020', 0)
 };
 
 module.exports = createReserve;
