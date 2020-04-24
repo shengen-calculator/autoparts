5d3193ac7383d34a04881fe6c9c2d3e7c07df25f
Vasyl Suddya <vasya.suddya@gmail.com>
2020-04-24 09:13:44 +0300

    fix for get orders, reserves sp

--- functions/integration/sp_web_getreservesbyvip.sql
+++ functions/integration/sp_web_getreservesbyvip.sql
@@ -4,20 +4,21 @@ AS
 BEGIN
 	SELECT TOP (100) [ID] as id
       ,[ID_Накладной] as invoiceId
-      ,[VIP] as vip
-      ,[Брэнд] as brand
+      ,TRIM([VIP]) as vip
+      ,TRIM([Брэнд]) as brand
       ,[Количество] as quantity
       ,[Цена] as euro
       ,[Грн] as uah
       ,[ID_Клиента] as clientId
       ,[ID_Запчасти] as productId
-      ,[Описание] as description
-      ,[Поставщик] as vendor
-      ,[Статус] as note
-      ,[Дата резерва] as 'date'
-      ,[Дата запроса] as orderDate
+      ,TRIM([Описание]) as description
+      ,TRIM([Поставщик]) as vendor
+      ,TRIM([Статус]) as note
+      ,FORMAT([Дата резерва], 'd', 'de-de') as 'date'
+      ,FORMAT([Дата запроса], 'd', 'de-de') as orderDate
       ,[Интернет] as source
-      ,[Номер поставщика] as number
+      ,TRIM([Номер поставщика]) as number
   FROM [dbo].[Накладные]
   WHERE  VIP like @vip AND [Обработано] = 0 AND [ID_Клиента] <> 378
 END
+
