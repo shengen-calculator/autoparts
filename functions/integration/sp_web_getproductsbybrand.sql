a422eaf68d740c195e484539022a6c6b43474ccb
Vasyl Suddya <Vasiliy.Suddya@eu.jll.com>
2020-04-26 14:58:00 +0300

    use correct price column for client

--- functions/integration/sp_web_getproductsbybrand.sql
+++ functions/integration/sp_web_getproductsbybrand.sql
@@ -2,127 +2,47 @@ CREATE PROCEDURE [dbo].[sp_web_getproductsbybrand]
 	@number varchar(25), @brand varchar(18), @clientId int
 AS
 BEGIN
-	DECLARE @analogId INT, @productId INT, @name VARCHAR(25), @firstBrand VARCHAR(18)
+	DECLARE @analogId INT, @productId INT,
+	    @name VARCHAR(25), @firstBrand VARCHAR(18),
+	    @cols AS NVARCHAR(MAX), @query  AS NVARCHAR(MAX)
 
 	SELECT
-	@firstBrand = shortNumber
+	@firstBrand = firstBrend
 	,@name = shortNumber
 	,@productId = productId
 	,@analogId = analogId
 	FROM getPartsByNumber(@number)
 	WHERE brand LIKE @brand
 
-
-	IF @analogId > 0
-		SELECT
-		ID_Запчасти AS id
+    SET @cols = N'ID_Запчасти AS id
 		,TRIM(Брэнд) AS brand
 		,TRIM([Сокращенное название]) AS vendor
 		,TRIM([Время заказа]) as orderTime
 	    ,TRIM([Время прихода]) as arrivalTime
 		,TRIM([Номер запчасти]) AS number
 		,Цена AS retail
+		,Цена AS retailEur
+		,' + dbo.GetClientPriceColumn(@clientId) + N'AS costEur
+		,Цена AS cost
 		,Доступно AS available
 		,Резерв AS reserve
 		,TRIM(Описание) AS description
 		,Заказ AS [order]
 		,Срок AS term
-		,Цена1 AS Price1
-		,Цена2 AS Price2
-		,Цена3 AS Price3
-		,Цена4 AS Price4
-		,Цена5 AS Price5
-		,Цена6 AS Price6
-		,Цена7 AS Price7
-		,Цена8 AS Price8
-		,Цена9 AS Price9
-		,Цена10 AS Price10
-		,Цена11 AS Price11
-		,Цена12 AS Price12
-		,Цена13 AS Price13
-		,Цена14 AS Price14
-		,Цена15 AS Price15
-		,Цена16 AS Price16
-		,Цена17 AS Price17
-		,FORMAT(Дата, 'd', 'de-de') AS date
+		,FORMAT(Дата, ''d'', ''de-de'') AS date
 		,[Не возвратный] AS ImpossibleReturn
 		,[Виден только менеджерам] AS OnlyForManagers
 		,IsEnsureDeliveryTerm AS isGuaranteedTerm
-		,IsQualityGuaranteed AS isGoodQuality
-		FROM GetPartsByAnalog(@firstBrand, @name, @analogId, @clientId)
+		,IsQualityGuaranteed AS isGoodQuality'
+
+
+	SET @query = N'SELECT ' + @cols
+	IF @analogId > 0
+	    SET @query = @query + N' FROM GetPartsByAnalog(''' + @firstBrand + N''', ''' + @name + N''', ' +  STR(@analogId) + N', ' +  STR(@clientId) + N')'
 	ELSE IF @productId > 0
-		SELECT
-		ID_Запчасти AS id
-		,TRIM(Брэнд) AS brand
-		,TRIM([Сокращенное название]) AS vendor
-        ,TRIM([Время заказа]) as orderTime
-        ,TRIM([Время прихода]) as arrivalTime
-		,TRIM([Номер запчасти]) AS number
-		,Цена AS retail
-		,Доступно AS available
-		,Резерв AS reserve
-		,TRIM(Описание) AS description
-		,Заказ AS [order]
-		,Срок AS term
-		,Цена1 AS Price1
-		,Цена2 AS Price2
-		,Цена3 AS Price3
-		,Цена4 AS Price4
-		,Цена5 AS Price5
-		,Цена6 AS Price6
-		,Цена7 AS Price7
-		,Цена8 AS Price8
-		,Цена9 AS Price9
-		,Цена10 AS Price10
-		,Цена11 AS Price11
-		,Цена12 AS Price12
-		,Цена13 AS Price13
-		,Цена14 AS Price14
-		,Цена15 AS Price15
-		,Цена16 AS Price16
-		,Цена17 AS Price17
-		,FORMAT(Дата, 'd', 'de-de') AS date
-		,[Не возвратный] AS ImpossibleReturn
-		,[Виден только менеджерам] AS OnlyForManagers
-		,IsEnsureDeliveryTerm AS isGuaranteedTerm
-		,IsQualityGuaranteed AS isGoodQuality
-		FROM GetPartsByShortNumber(@firstBrand, @name, @clientId)
+        SET @query = @query + N' FROM GetPartsByShortNumber(''' + @firstBrand + N''', ''' + @name + N''', ' +  STR(@clientId) + N')'
 	ELSE
-        SELECT
-		ID_Запчасти AS id
-		,TRIM(Брэнд) AS brand
-		,TRIM([Сокращенное название]) AS vendor
-        ,TRIM([Время заказа]) as orderTime
-        ,TRIM([Время прихода]) as arrivalTime
-		,TRIM([Номер запчасти]) AS number
-		,Цена AS retail
-		,Доступно AS available
-		,Резерв AS reserve
-		,TRIM(Описание) AS description
-		,Заказ AS [order]
-		,Срок AS term
-		,Цена1 AS Price1
-		,Цена2 AS Price2
-		,Цена3 AS Price3
-		,Цена4 AS Price4
-		,Цена5 AS Price5
-		,Цена6 AS Price6
-		,Цена7 AS Price7
-		,Цена8 AS Price8
-		,Цена9 AS Price9
-		,Цена10 AS Price10
-		,Цена11 AS Price11
-		,Цена12 AS Price12
-		,Цена13 AS Price13
-		,Цена14 AS Price14
-		,Цена15 AS Price15
-		,Цена16 AS Price16
-		,Цена17 AS Price17
-		,FORMAT(Дата, 'd', 'de-de') AS date
-		,[Не возвратный] AS ImpossibleReturn
-		,[Виден только менеджерам] AS OnlyForManagers
-		,IsEnsureDeliveryTerm AS isGuaranteedTerm
-		,IsQualityGuaranteed AS isGoodQuality
-		FROM GetPartsByBrand(@firstBrand, @name, @clientId)
+        SET @query = @query + N' FROM GetPartsByBrand(''' + @firstBrand + N''', ''' + @name + N''', ' + STR(@clientId) + N')'
+    exec sp_executesql @query
 
 END
