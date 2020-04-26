b2b295ff0eb699c270a6cf6e5890490015d907b1
Vasyl Suddya <Vasiliy.Suddya@eu.jll.com>
2020-04-26 16:39:19 +0300

    convert price to uah, by current rate

--- functions/integration/sp_web_getproductsbybrand.sql
+++ functions/integration/sp_web_getproductsbybrand.sql
@@ -20,10 +20,10 @@ BEGIN
 		,TRIM([Время заказа]) as orderTime
 	    ,TRIM([Время прихода]) as arrivalTime
 		,TRIM([Номер запчасти]) AS number
-		,Цена AS retail
+		,Цена * ' + STR(dbo.GetUahRate()) + N' AS retail
 		,Цена AS retailEur
-		,' + dbo.GetClientPriceColumn(@clientId) + N'AS costEur
-		,Цена AS cost
+		,' + dbo.GetClientPriceColumn(@clientId) + N' AS costEur
+		,' + dbo.GetClientPriceColumn(@clientId) + N'*' + STR(dbo.GetUahRate()) + N' AS cost
 		,Доступно AS available
 		,Резерв AS reserve
 		,TRIM(Описание) AS description
