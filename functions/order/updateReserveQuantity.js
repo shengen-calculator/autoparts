47b53e825124d29a687f5e1ea42f575428d074ef
Vasyl Suddya <vasya.suddya@gmail.com>
2020-04-30 08:51:01 +0300

    update reserve quantity sp, integration

--- functions/order/updateReserveQuantity.js
+++ functions/order/updateReserveQuantity.js
@@ -11,6 +11,20 @@ const updateReserveQuantity = async (data, context) => {
         throw new functions.https.HttpsError('invalid-argument',
             'The function must be called with argument "reserve Id and quantity"');
     }
+
+    try {
+        const pool = await sql.connect(config);
+
+        await pool.request()
+                .input('reserveId', sql.Int, data.reserveId)
+            .input('quantity', sql.Int, data.quantity)
+            .execute('sp_web_updatereserveqty');
+    } catch (err) {
+        if(err) {
+            throw new functions.https.HttpsError('internal',
+                err.message);
+        }
+    }
 };
 
 module.exports = updateReserveQuantity;
