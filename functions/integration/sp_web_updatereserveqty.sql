e4a28ad865522ffdb25b6b98b2047cd1adb0aa72
Vasyl Suddya <vasya.suddya@gmail.com>
2020-05-02 08:47:35 +0300

    remove oldQuantity from parameter list, update reserve  quantity

--- functions/integration/sp_web_updatereserveqty.sql
+++ functions/integration/sp_web_updatereserveqty.sql
@@ -1,16 +1,20 @@
 CREATE PROCEDURE [dbo].[sp_web_updatereserveqty]
-    @reserveId INT, @oldQuantity INT, @newQuantity INT, @productId INT
+    @reserveId INT, @quantity INT, @productId INT
 AS
 BEGIN
-    DECLARE @balance INT
+    DECLARE @balance INT, @oldQuantity INT
 
     BEGIN TRANSACTION;
+        SELECT @oldQuantity = Количество
+        FROM   dbo.[Подчиненная накладные]
+        WHERE  (ID = @reserveId)
 
         SELECT  @balance = Остаток
         FROM    dbo.Остаток_
         WHERE  (ID_Запчасти = @productId)
-        IF(@balance + @oldQuantity > @newQuantity)
-            UPDATE dbo.[Подчиненная накладные] SET Количество = @newQuantity  WHERE ID = @reserveId
+
+        IF(@balance + @oldQuantity > @quantity)
+            UPDATE dbo.[Подчиненная накладные] SET Количество = @quantity  WHERE ID = @reserveId
         ELSE
             RAISERROR ('Quantity update operation error. Quantity is not enough', 16, 1)
 
@@ -18,3 +22,4 @@ BEGIN
 
 END
 go
+
--- functions/order/updateReserveQuantity.js
+++ functions/order/updateReserveQuantity.js
@@ -7,9 +7,9 @@ const updateReserveQuantity = async (data, context) => {
 
     util.CheckForManagerRole(context);
 
-    if (!data || !data.reserveId || typeof data.oldQuantity === 'undefined' ||  typeof data.newQuantity === 'undefined' || !data.productId) {
+    if (!data || !data.reserveId || typeof data.quantity === 'undefined' || !data.productId) {
         throw new functions.https.HttpsError('invalid-argument',
-            'The function must be called with argument "reserveId, oldQuantity, newQuantity and productId"');
+            'The function must be called with argument "reserveId, quantity and productId"');
     }
 
     try {
@@ -17,8 +17,7 @@ const updateReserveQuantity = async (data, context) => {
 
         await pool.request()
             .input('reserveId', sql.Int, data.reserveId)
-            .input('oldQuantity', sql.Int, data.oldQuantity)
-            .input('newQuantity', sql.Int, data.newQuantity)
+            .input('quantity', sql.Int, data.quantity)
             .input('productId', sql.Int, data.productId)
             .execute('sp_web_updatereserveqty');
     } catch (err) {
