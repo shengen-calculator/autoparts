34044ed388a958122e08ebe8cb663a8b09465844
Vasyl Suddya <vasya.suddya@gmail.com>
2020-05-02 08:30:04 +0300

    use transaction for update quantity reserve operation

--- functions/order/updateReserveQuantity.js
+++ functions/order/updateReserveQuantity.js
@@ -7,17 +7,19 @@ const updateReserveQuantity = async (data, context) => {
 
     util.CheckForManagerRole(context);
 
-    if (!data || !data.reserveId || typeof data.quantity === 'undefined') {
+    if (!data || !data.reserveId || typeof data.oldQuantity === 'undefined' ||  typeof data.newQuantity === 'undefined' || !data.productId) {
         throw new functions.https.HttpsError('invalid-argument',
-            'The function must be called with argument "reserve Id and quantity"');
+            'The function must be called with argument "reserveId, oldQuantity, newQuantity and productId"');
     }
 
     try {
         const pool = await sql.connect(config);
 
         await pool.request()
-                .input('reserveId', sql.Int, data.reserveId)
-            .input('quantity', sql.Int, data.quantity)
+            .input('reserveId', sql.Int, data.reserveId)
+            .input('oldQuantity', sql.Int, data.oldQuantity)
+            .input('newQuantity', sql.Int, data.newQuantity)
+            .input('productId', sql.Int, data.productId)
             .execute('sp_web_updatereserveqty');
     } catch (err) {
         if(err) {
