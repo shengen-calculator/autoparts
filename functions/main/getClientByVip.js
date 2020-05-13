989c65a0add884c9802af73eb6cb486d6112bb1d
Vasyl Suddya <vasya.suddya@gmail.com>
2020-05-13 17:46:19 +0300

    getClientByVip modify for using by client too

--- functions/main/getClientByVip.js
+++ functions/main/getClientByVip.js
@@ -5,18 +5,17 @@ const config = require('../mssql.connection').config;
 
 const getClientByVip = async (data, context) => {
 
-    util.checkForManagerRole(context);
-
-    if (!data) {
-        throw new functions.https.HttpsError('invalid-argument',
-            'The function must be called with one argument "vip"');
+    if (data) {
+        util.checkForManagerRole(context);
+    } else {
+        util.checkForClientRole(context);
     }
 
     try {
         const pool = await sql.connect(config);
 
         const result = await pool.request()
-            .input('vip', sql.VarChar(10), data)
+            .input('vip', sql.VarChar(10), data ? data : context.auth.token.vip)
             .execute('sp_web_getclientbyvip');
         return result.recordset;
     } catch (err) {
