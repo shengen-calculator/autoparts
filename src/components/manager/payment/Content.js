7caa8c917a6cae7d85a4d3cba2fc606e9aa95f0e
Vasyl Suddya <Vasiliy.Suddya@eu.jll.com>
2020-05-18 19:47:51 +0300

    payment refactoring, move common code to PaymentPage

--- src/components/manager/payment/Content.js
+++ src/components/manager/payment/Content.js
@@ -1,18 +1,14 @@
 import React, {useEffect} from 'react';
 import PropTypes from 'prop-types';
-import AppBar from '@material-ui/core/AppBar';
-import Paper from '@material-ui/core/Paper';
 import {withStyles} from '@material-ui/core/styles';
 import Header from "../Header";
 import Copyright from "../../common/Copyright";
-import PaymentTable from "../../common/Tables/PaymentTable";
 import {connect} from "react-redux";
-import Typography from "@material-ui/core/Typography";
 import {useParams} from "react-router-dom";
 import {getPayments} from "../../../redux/actions/clientActions";
 import {Helmet} from "react-helmet";
 import PaymentStyle from "../../common/Tables/PaymentStyle";
-import {formatCurrency} from "../../../util/Formatter";
+import PaymentPage from "../../common/PaymentPage";
 
 const styles = theme => PaymentStyle(theme);
 
@@ -34,28 +30,7 @@ function Content({client, calls, getPayments, ...props}) {
             <Helmet>
                 <title>Fenix - План платежів - {client.vip}</title>
             </Helmet>
-            <main className={classes.main}>
-                {calls === 0 &&
-                <Paper className={classes.paper}>
-                    <AppBar className={classes.searchBar} position="static" color="default" elevation={0}/>
-                    <div className={classes.contentWrapper}>
-                        {isTableShown ?
-                            (debtAmount > 0) ?
-                                <PaymentTable payments={client.payments} debt={debtAmount}/> :
-                                <Typography className={classes.advance} align="center">
-                                    {`На Вашому рахунку: ${formatCurrency(Math.abs(debtAmount), client.isEuroClient ? 'EUR' : 'UAH')}`}
-                                </Typography>
-                            :
-                            client.payments.length > 0 &&
-                            <Typography className={client.payments[0].amount > 0 ? classes.debt : classes.advance} align="center">
-                                {client.payments[0].amount > 0 ?
-                                    `Ваш борг складає: ${formatCurrency(client.payments[0].amount, client.isEuroClient ? 'EUR' : 'UAH')}` :
-                                    `На Вашому рахунку: ${formatCurrency(Math.abs(client.payments[0].amount), client.isEuroClient ? 'EUR' : 'UAH')}`}
-                            </Typography>
-                        }
-                    </div>
-                </Paper>}
-            </main>
+            <PaymentPage debtAmount={debtAmount} client={client} calls={calls} isTableShown={isTableShown}/>
             <footer className={classes.footer}>
                 <Copyright/>
             </footer>
