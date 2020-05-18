0679f98a980f46d91029cac0b5d7c4094240a565
Vasyl Suddya <Vasiliy.Suddya@eu.jll.com>
2020-05-18 19:47:51 +0300

    payment refactoring, move common code to PaymentPage

--- src/components/client/payment/Content.js
+++ src/components/client/payment/Content.js
@@ -1,18 +1,13 @@
 import React, {useEffect} from 'react';
 import PropTypes from 'prop-types';
-import AppBar from '@material-ui/core/AppBar';
-import Paper from '@material-ui/core/Paper';
 import {withStyles} from '@material-ui/core/styles';
 import Header from "../Header";
 import Copyright from "../../common/Copyright";
 import {connect} from "react-redux";
-import Typography from "@material-ui/core/Typography";
 import {getPayments} from "../../../redux/actions/clientActions";
 import {Helmet} from "react-helmet";
-import PaymentTable from "../../common/Tables/PaymentTable";
 import PaymentStyle from "../../common/Tables/PaymentStyle";
-import {formatCurrency} from "../../../util/Formatter";
-
+import PaymentPage from "../../common/PaymentPage";
 
 const styles = theme => PaymentStyle(theme);
 
@@ -25,7 +20,6 @@ function Content({client, calls, getPayments, ...props}) {
         }
     }, [client.isPaymentsLoaded, getPayments]);
 
-
     const isTableShown = client && client.payments && client.payments.length > 1;
     const debtAmount = isTableShown ? client.payments.reduce((a, b) => a + b.amount, 0) : 0;
 
@@ -35,28 +29,7 @@ function Content({client, calls, getPayments, ...props}) {
             <Helmet>
                 <title>Fenix - План платежів</title>
             </Helmet>
-            <main className={classes.main}>
-                {calls === 0 &&
-                <Paper className={classes.paper}>
-                    <AppBar className={classes.searchBar} position="static" color="default" elevation={0}/>
-                    <div className={classes.contentWrapper}>
-                        {isTableShown ?
-                            (debtAmount > 0) ?
-                                <PaymentTable payments={client.payments} debt={debtAmount}/> :
-                                <Typography className={classes.advance} align="center">
-                                    {`На Вашому рахунку: ${formatCurrency(Math.abs(debtAmount), client.isEuroClient ? 'EUR' : 'UAH')}`}
-                                </Typography>
-                            :
-                            client.payments.length > 0 &&
-                            <Typography className={client.payments[0].amount > 0 ? classes.debt : classes.advance} align="center">
-                                {client.payments[0].amount > 0 ?
-                                    `Ваш борг складає: ${formatCurrency(client.payments[0].amount, client.isEuroClient ? 'EUR' : 'UAH')}` :
-                                    `На Вашому рахунку: ${formatCurrency(Math.abs(client.payments[0].amount), client.isEuroClient ? 'EUR' : 'UAH')}`}
-                            </Typography>
-                        }
-                    </div>
-                </Paper>}
-            </main>
+            <PaymentPage debtAmount={debtAmount} client={client} calls={calls} isTableShown={isTableShown}/>
             <footer className={classes.footer}>
                 <Copyright/>
             </footer>
---
+++ src/components/common/PaymentPage.js
@@ -0,0 +1,40 @@
+import Paper from "@material-ui/core/Paper";
+import AppBar from "@material-ui/core/AppBar";
+import PaymentTable from "./Tables/PaymentTable";
+import Typography from "@material-ui/core/Typography";
+import {formatCurrency} from "../../util/Formatter";
+import React from "react";
+import PaymentStyle from "./Tables/PaymentStyle";
+import {withStyles} from "@material-ui/core/styles";
+
+const styles = theme => PaymentStyle(theme);
+
+function PaymentPage({debtAmount, client, calls, isTableShown, ...props}) {
+    const {classes} = props;
+    return (
+        <main className={classes.main}>
+            {calls === 0 &&
+            <Paper className={classes.paper}>
+                <AppBar className={classes.searchBar} position="static" color="default" elevation={0}/>
+                <div className={classes.contentWrapper}>
+                    {isTableShown ?
+                        (debtAmount > 0) ?
+                            <PaymentTable payments={client.payments} debt={debtAmount}/> :
+                            <Typography className={classes.advance} align="center">
+                                {`На Вашому рахунку: ${formatCurrency(Math.abs(debtAmount), client.isEuroClient ? 'EUR' : 'UAH')}`}
+                            </Typography>
+                        :
+                        client.payments.length > 0 &&
+                        <Typography className={client.payments[0].amount > 0 ? classes.debt : classes.advance} align="center">
+                            {client.payments[0].amount > 0 ?
+                                `Ваш борг складає: ${formatCurrency(client.payments[0].amount, client.isEuroClient ? 'EUR' : 'UAH')}` :
+                                `На Вашому рахунку: ${formatCurrency(Math.abs(client.payments[0].amount), client.isEuroClient ? 'EUR' : 'UAH')}`}
+                        </Typography>
+                    }
+                </div>
+            </Paper>}
+        </main>
+    )
+}
+
+export default withStyles(styles)(PaymentPage);
