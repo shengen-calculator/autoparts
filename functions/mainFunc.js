58eda9ae96a040dc262426c780042cadeb2e62e7
Vasyl Suddya <vasya.suddya@gmail.com>
2020-06-06 08:22:44 +0300

    create sp and func

--- functions/mainFunc.js
+++ functions/mainFunc.js
@@ -2,6 +2,7 @@ const functions = require('firebase-functions');
 const processSignUp = require('./main/processSignUp');
 const getClientByVip = require('./main/getClientByVip');
 const getPaymentsByVip = require('./main/getPaymentsByVip');
+const getCurrencyRate = require('./main/getCurrencyRate');
 
 exports.processSignUp = functions.region('europe-west1').auth.user().onCreate((user) => {
     return processSignUp(user);
@@ -13,4 +14,8 @@ exports.getClientByVip = functions.region('europe-west1').https.onCall(async (da
 
 exports.getPaymentsByVip = functions.region('europe-west1').https.onCall(async (data, context) => {
     return getPaymentsByVip(data, context);
+});
+
+exports.getCurrencyRate = functions.region('europe-west1').https.onCall(async (data, context) => {
+    return getCurrencyRate(data, context);
 });
