c326891b8f98bc649b5c2287ec9a74a892a2bf17
Vasyl Suddya <Vasiliy.Suddya@eu.jll.com>
2020-06-25 15:22:30 +0300

    sp and func -> check if order already present

--- functions/search/checkIfPresentInOrderList.js
+++ functions/search/checkIfPresentInOrderList.js
@@ -1,22 +1,30 @@
 const functions = require('firebase-functions');
 const util = require('../util');
+const sql = require('mssql');
+const config = require('../mssql.connection').config;
 
 const checkIfPresentInOrderList = async (data, context) => {
-
     util.checkForManagerRole(context);
 
-    if (!data) {
+    if (!data || !data.productId) {
         throw new functions.https.HttpsError('invalid-argument',
-            'The function must be called with one argument "AnalogId"');
+            'The function must be called with the "ProductId" argument');
     }
 
-    function createData(id, vip, brand, number, quantity, alternative, orderDate, isUrgent, date) {
-        return {id, vip, brand, number, quantity, alternative, orderDate, isUrgent, date };
+    try {
+        const pool = await sql.connect(config);
+
+        const result = await pool.request()
+            .input('productId', sql.Int, data.productId)
+            .execute('sp_web_checkifpresentinorderlist');
+        return result.recordset;
+    } catch (err) {
+        if(err) {
+            throw new functions.https.HttpsError('internal', err.message);
+        }
+        return {err: err.message};
     }
 
-    return [
-        createData(1, '0020', 'FAG', 'FAG713610230', 4, '', '11.02.2020', false, '11.02.2020')
-    ];
 };
 
-module.exports = checkIfPresentInOrderList;
+module.exports = createOrder;
