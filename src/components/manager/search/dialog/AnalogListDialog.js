c1a34b7f33102cdb50af105bf7252db970e3f7c1
Vasyl Suddya <Vasiliy.Suddya@eu.jll.com>
2020-07-02 15:36:06 +0300

    show list of analogs in table

--- src/components/manager/search/dialog/AnalogListDialog.js
+++ src/components/manager/search/dialog/AnalogListDialog.js
@@ -6,10 +6,23 @@ import {connect} from "react-redux";
 import Button from "@material-ui/core/Button";
 import DialogActions from "@material-ui/core/DialogActions";
 import Box from '@material-ui/core/Box';
+import Table from "@material-ui/core/Table";
+import TableHead from "@material-ui/core/TableHead";
+import TableRow from "@material-ui/core/TableRow";
+import TableCell from "@material-ui/core/TableCell";
+import TableBody from "@material-ui/core/TableBody";
+import DialogContent from "@material-ui/core/DialogContent";
 
 function AnalogListDialog(props) {
     const {isOpened, onClose, analogs, calls} = props;
-
+    const sorted = analogs.concat().sort((a, b) => {
+        if(a['stock'] < b['stock']) {
+            return 1;
+        } else if(a['stock'] === b['stock'] && a.price < b.price) {
+            return 1;
+        }
+        return -1;
+    });
     return (
         <div>
             <Dialog open={isOpened} aria-labelledby="form-dialog-title" onClose={onClose} maxWidth="xl">
@@ -19,7 +32,37 @@ function AnalogListDialog(props) {
                 <Box display="flex" justifyContent="center">
                     { (analogs.length === 0 && calls > 0) ?
                         <CircularProgress /> :
-                        <span>{analogs.length}</span>
+                        <DialogContent>
+                            <Table aria-label="simple table"  size="small">
+                                <TableHead>
+                                    <TableRow>
+                                        <TableCell align="right">Пост.</TableCell>
+                                        <TableCell align="right">Бренд</TableCell>
+                                        <TableCell align="right">Номер</TableCell>
+                                        <TableCell align="right">Оптова</TableCell>
+                                        <TableCell align="right">Роздрібна</TableCell>
+                                        <TableCell align="right">Знижка</TableCell>
+                                        <TableCell align="right">Залишок</TableCell>
+                                    </TableRow>
+                                </TableHead>
+                                <TableBody>
+                                    {sorted.map((row, index) => {
+                                        return (
+                                            <TableRow key={index}>
+                                                <TableCell align="right">{row.vendor}</TableCell>
+                                                <TableCell align="right">{row.brand}</TableCell>
+                                                <TableCell align="right">{row.number}</TableCell>
+                                                <TableCell align="right">{row.price}</TableCell>
+                                                <TableCell align="right">{row.retail}</TableCell>
+                                                <TableCell align="right">{row.discount}</TableCell>
+                                                <TableCell align="right">{row['stock']}</TableCell>
+                                            </TableRow>
+                                        )
+                                    } )}
+                                </TableBody>
+                            </Table>
+
+                        </DialogContent>
                     }
 
                 </Box>
