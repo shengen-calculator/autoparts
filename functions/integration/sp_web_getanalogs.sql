a4da52cc96d35ec4609ada528f028f94550e427f
Vasyl Suddya <Vasiliy.Suddya@eu.jll.com>
2020-07-02 14:57:14 +0300

    fix get analogs sp (serch by brand and number)

--- functions/integration/sp_web_getanalogs.sql
+++ functions/integration/sp_web_getanalogs.sql
@@ -7,31 +7,30 @@ BEGIN
             FROM dbo.[Каталог запчастей]
                      INNER JOIN
                  dbo.Брэнды ON dbo.[Каталог запчастей].ID_Брэнда = dbo.Брэнды.ID_Брэнда
-            WHERE (dbo.[Каталог запчастей].[Номер поставщика] = @number)
+            WHERE (dbo.[Каталог запчастей].NAME = @number)
               AND (dbo.Брэнды.Брэнд = @brand)
             ORDER BY dbo.[Каталог запчастей].ID_аналога DESC
 
 
         END
-    ELSE
-        BEGIN
-            SELECT TOP (100) dbo.[Каталог запчастей].ID_Запчасти             AS productId,
-                             RTRIM(dbo.Поставщики.[Сокращенное название])    AS vendor,
-                             RTRIM(dbo.Брэнды.Брэнд)                         AS brand,
-                             RTRIM(dbo.[Каталог запчастей].[Номер запчасти]) AS number,
-                             dbo.[Каталог запчастей].Цена7                   AS price,
-                             dbo.[Каталог запчастей].Цена                    AS retail,
-                             dbo.[Каталог запчастей].Скидка                  AS discount,
-                             ISNULL(dbo.Остаток_.Остаток, 0)                 AS stock
-            FROM dbo.[Каталог запчастей]
-                     INNER JOIN
-                 dbo.Брэнды ON dbo.[Каталог запчастей].ID_Брэнда = dbo.Брэнды.ID_Брэнда
-                     INNER JOIN
-                 dbo.Поставщики ON dbo.[Каталог запчастей].ID_Поставщика = dbo.Поставщики.ID_Поставщика
-                     LEFT OUTER JOIN
-                 dbo.Остаток_ ON dbo.[Каталог запчастей].ID_Запчасти = dbo.Остаток_.ID_Запчасти
-            WHERE (dbo.[Каталог запчастей].ID_аналога = @analogId)
-        END
+
+    SELECT TOP (100) dbo.[Каталог запчастей].ID_Запчасти             AS productId,
+                     RTRIM(dbo.Поставщики.[Сокращенное название])    AS vendor,
+                     RTRIM(dbo.Брэнды.Брэнд)                         AS brand,
+                     RTRIM(dbo.[Каталог запчастей].[Номер запчасти]) AS number,
+                     dbo.[Каталог запчастей].Цена7                   AS price,
+                     dbo.[Каталог запчастей].Цена                    AS retail,
+                     dbo.[Каталог запчастей].Скидка                  AS discount,
+                     ISNULL(dbo.Остаток_.Остаток, 0)                 AS stock
+    FROM dbo.[Каталог запчастей]
+             INNER JOIN
+         dbo.Брэнды ON dbo.[Каталог запчастей].ID_Брэнда = dbo.Брэнды.ID_Брэнда
+             INNER JOIN
+         dbo.Поставщики ON dbo.[Каталог запчастей].ID_Поставщика = dbo.Поставщики.ID_Поставщика
+             LEFT OUTER JOIN
+         dbo.Остаток_ ON dbo.[Каталог запчастей].ID_Запчасти = dbo.Остаток_.ID_Запчасти
+    WHERE (dbo.[Каталог запчастей].ID_аналога = @analogId)
+
 END
 go
 
