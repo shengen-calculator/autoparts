cd0e2aba95b701cade9ae5624ba3e9f0efa5665f
Vasyl Suddya <Vasiliy.Suddya@eu.jll.com>
2020-07-08 13:16:15 +0300

    small fix

--- src/sagas/applicationSaga.js
+++ src/sagas/applicationSaga.js
@@ -6,7 +6,7 @@ export function* updateAppState(action) {
     try {
         yield put({type: types.BEGIN_API_CALL});
         yield call(AppStateApi.updateApplicationState, action.state);
-        yield put({type: types.LOAD_RESERVES_SUCCESS});
+        yield put({type: types.UPDATE_APP_STATE_SUCCESS});
     } catch (error) {
         yield put({type: types.API_CALL_ERROR});
         yield put({type: types.UPDATE_APP_STATE_FAILURE, text: error.message})
