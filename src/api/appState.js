f912aa73e6d2aec29cb02dcbffded1e77be9084e
Vasyl Suddya <Vasiliy.Suddya@eu.jll.com>
2020-07-08 13:10:51 +0300

    wrapper for realtime db set func

--- src/api/appState.js
+++ src/api/appState.js
@@ -1,9 +1,15 @@
 import {database} from "./database";
 
 class AppStateApi {
-    static updateApplicationState(state) {
-        database.ref('app-settings').set(state);
-    }
+    static updateApplicationState = function (state) {
+        return new Promise(function (resolve, reject) {
+            database.ref('app-settings').set(state).then(() => {
+                resolve();
+            }).catch((err) => {
+                reject(new Error(err.message));
+            });
+        });
+    };
 
     static subscribeAppStateUpdate(handler, errorHandler) {
         return database.ref('app-settings').on('child_changed', function (snapshot) {
