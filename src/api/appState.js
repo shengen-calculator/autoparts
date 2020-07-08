80049674c557da08671178918539248f2a382885
Vasyl Suddya <Vasiliy.Suddya@eu.jll.com>
2020-07-08 07:52:35 +0300

    add app state update listener to api

--- src/api/appState.js
+++ src/api/appState.js
@@ -4,6 +4,14 @@ class AppStateApi {
     static updateApplicationState(state) {
         database.ref('app-settings').set(state);
     }
+
+    static subscribeAppStateUpdate(handler, errorHandler) {
+        return database.ref('app-settings').on('child_changed', function (snapshot) {
+            handler(snapshot);
+        }, function (error) {
+            errorHandler(error);
+        })
+    }
 }
 
 export default AppStateApi;
--- src/sagas/applicationSaga.js
+++ src/sagas/applicationSaga.js
@@ -5,8 +5,8 @@ import AppStateApi from "../api/appState";
 export function* updateAppState(action) {
     try {
         yield put({type: types.BEGIN_API_CALL});
-        const {data} = yield call(AppStateApi.updateApplicationState, action.state);
-        yield put({type: types.UPDATE_APP_STATE_SUCCESS, state: data});
+        yield call(AppStateApi.updateApplicationState, action.state);
+        yield put({type: types.UPDATE_APP_STATE_SUCCESS});
     } catch (e) {
         yield put({type: types.API_CALL_ERROR});
         yield put({type: types.UPDATE_APP_STATE_FAILURE, text: e.message});
