6e7bb63920c310105f47541db45bfd89fd7f2a81
Vasyl Suddya <Vasiliy.Suddya@eu.jll.com>
2020-07-08 13:15:15 +0300

    add ajax spinner to state update operation

--- src/sagas/applicationSaga.js
+++ src/sagas/applicationSaga.js
@@ -4,8 +4,11 @@ import AppStateApi from "../api/appState";
 
 export function* updateAppState(action) {
     try {
+        yield put({type: types.BEGIN_API_CALL});
         yield call(AppStateApi.updateApplicationState, action.state);
+        yield put({type: types.LOAD_RESERVES_SUCCESS});
     } catch (error) {
+        yield put({type: types.API_CALL_ERROR});
         yield put({type: types.UPDATE_APP_STATE_FAILURE, text: error.message})
     }
 }
