af5741fba13c8c9f7ecd839ea44099c2b39eb4bf
Vasyl Suddya <Vasiliy.Suddya@eu.jll.com>
2020-07-08 07:39:10 +0300

    remove update app func

--- functions/settings.example.js
+++ functions/settings.example.js
@@ -1,8 +1,5 @@
 const configuration = {
     bucketId: 'someproject-1234567.appspot.com',
-    apiKey: "XXXXXXXXXXXXXXXXXXXXXX",
-    authDomain: "someproject-1234567.firebaseapp.com",
-    databaseURL: "https://someproject-1234567.firebaseio.com"
 };
 
 module.exports = configuration;
