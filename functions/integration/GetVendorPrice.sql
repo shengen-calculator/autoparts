479cacbab9b308a3668983fa44a627c90653feb9
Vasyl Suddya <vasya.suddya@gmail.com>
2020-08-23 23:16:00 +0300

    fix sql func GetVendorPrice, use SupplierWarehouse table to get Vendor

--- functions/integration/GetVendorPrice.sql
+++ functions/integration/GetVendorPrice.sql
@@ -4,13 +4,17 @@ AS
 BEGIN
     DECLARE @vendorPrice DECIMAL(12, 2)
     SELECT @vendorPrice = MAX(dbo.[Каталоги поставщиков].Цена7)
+
     FROM dbo.[Каталоги поставщиков]
              INNER JOIN
-         dbo.[Каталог запчастей] ON dbo.[Каталоги поставщиков].ID_Поставщика = dbo.[Каталог запчастей].ID_Поставщика AND
-                                    dbo.[Каталоги поставщиков].Name = dbo.[Каталог запчастей].namepost
+         dbo.[Каталог запчастей] ON dbo.[Каталоги поставщиков].Name = dbo.[Каталог запчастей].namepost
              INNER JOIN
          dbo.Брэнды ON dbo.[Каталог запчастей].ID_Брэнда = dbo.Брэнды.ID_Брэнда AND
                        dbo.[Каталоги поставщиков].Брэнд = dbo.Брэнды.Брэнд
+             INNER JOIN
+         dbo.SupplierWarehouse ON dbo.[Каталоги поставщиков].WarehouseId = dbo.SupplierWarehouse.Id AND
+                                  dbo.[Каталог запчастей].ID_Поставщика = dbo.SupplierWarehouse.SupplierId
+
     GROUP BY dbo.[Каталог запчастей].ID_Запчасти
     HAVING (dbo.[Каталог запчастей].ID_Запчасти = @productId)
 
