382e71717908f51849e56526188f6e31498c2ecc
Vasyl Suddya <vasya.suddya@gmail.com>
2020-10-27 23:21:16 +0200

    change color scheme for order table

--- src/components/common/OrderRowStyle.js
+++ src/components/common/OrderRowStyle.js
@@ -1,6 +1,19 @@
-import pink from '@material-ui/core/colors/pink';
 import teal from '@material-ui/core/colors/teal';
+import purple from "@material-ui/core/colors/purple";
+import orange from "@material-ui/core/colors/orange";
+import red from "@material-ui/core/colors/red";
 
-export const getOrderRowClass = (row) => row.status === 0 ? {backgroundColor: teal[50]} :
-    (row.status === 2 || row.status === 3 || row.status === 4) ? {backgroundColor: pink[50]} : {};
-
+export const getOrderRowClass = (row) => {
+    switch (row.status) {
+        case 0:
+            return {backgroundColor: teal[50]};
+        case 4:
+            return {backgroundColor: red[500]};
+        case 3:
+            return {backgroundColor: orange[200]};
+        case 2:
+            return {backgroundColor: purple[200]};
+        default:
+            return {}
+    }
+};
