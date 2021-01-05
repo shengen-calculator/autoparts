52ad7eeb26566c2869bb44d33aff6ceaf3a72207
Vasyl Suddya <Vasiliy.Suddya@eu.jll.com>
2021-01-05 22:32:49 +0200

    fix update price stored procedure

--- functions/integration/sp_web_updateprice.sql
+++ functions/integration/sp_web_updateprice.sql
@@ -10,15 +10,14 @@ BEGIN
         Цена            = IIF(@price = 0, NULL, @retail),
         Цена4           = IIF(@price = 0, NULL, @price / 1.04),
         Цена14          = IIF(@price = 0, NULL, @price / 1.02),
-        Цена5           = IIF(@price = 0, NULL, @retail * 0.75),
-        Цена6           = IIF(@price = 0, NULL, @retail * 0.7),
-        Цена1           = IIF(@price = 0, NULL, @retail * 0.95),
-        Цена2           = IIF(@price = 0, NULL, @retail * 0.9),
-        Цена3           = IIF(@price = 0, NULL, @retail * 0.85),
+        Цена5           = IIF(@price = 0, NULL, (1 - (1 - 0.75) * @discount / 35) * @retail),
+        Цена6           = IIF(@price = 0, NULL, (1 - (1 - 0.7) * @discount / 35) * @retail),
+        Цена1           = IIF(@price = 0, NULL, (1 - (1 - 0.95) * @discount / 35) * @retail),
+        Цена2           = IIF(@price = 0, NULL, (1 - (1 - 0.9) * @discount / 35) * @retail),
+        Цена3           = IIF(@price = 0, NULL, (1 - (1 - 0.85) * @discount / 35) * @retail),
         Цена7           = IIF(@price = 0, NULL, @price),
         Цена13          = IIF(@price = 0, NULL, @price),
         Цена_обработана = IIF(@price = 0, 0, 1)
     WHERE ID_Запчасти = @productId
 END
 go
-
