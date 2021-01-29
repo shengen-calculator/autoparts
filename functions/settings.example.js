b8c7cc74fe7d87836d2f03eb2fbfd056135fbb3e
Vasyl Suddya <suddya@toptal.com>
2021-01-29 19:39:13 +0200

    first implementation for getAttachment endpoint

--- functions/settings.example.js
+++ functions/settings.example.js
@@ -1,5 +1,11 @@
 const configuration = {
     bucketId: 'someproject-1234567.appspot.com',
+    photos: "autoparts-1234",
+    imapUser: 'mygmailname@gmail.com',
+    imapPassword: 'mygmailpassword',
+    imapHost: 'imap.gmail.com',
+    imapPort: 993,
+    imapTls: true
 };
 
 module.exports = configuration;
