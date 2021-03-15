50919ac01f12f8701d3b01d2d0dd1b93715e9d51
Vasyl Suddya <vasya.suddya@gmail.com>
2021-03-15 15:50:50 +0200

    mark 0-therm position by green

--- src/components/common/Tables/SearchTableRow.js
+++ src/components/common/Tables/SearchTableRow.js
@@ -10,6 +10,7 @@ import {withStyles} from '@material-ui/core/styles';
 import Tooltip from "@material-ui/core/Tooltip";
 import Typography from '@material-ui/core/Typography';
 import {RoleEnum} from "../../../util/Enums";
+import teal from "@material-ui/core/colors/teal";
 
 export const headCells = [
     {id: 'vendor', numeric: false, disablePadding: false, label: 'Пост.'},
@@ -48,6 +49,7 @@ export default function SearchTableRow(row, index, isSelected, handleClick, isEu
             aria-checked={isItemSelected}
             tabIndex={-1}
             key={row.id}
+            style={row['term'] === 0 ? {backgroundColor: teal[50]} : {}}
             selected={isItemSelected}
         >
             {RoleEnum.Manager === role &&  <TableCell width="10%" align="left" name="order" style={pointer}>{row.vendor}</TableCell>}
