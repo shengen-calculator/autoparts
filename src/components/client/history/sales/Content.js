33896a80eda4a5273fdc2010a68ed674979e48de
Vasyl Suddya <vasya.suddya@gmail.com>
2021-04-04 22:08:12 +0300

    first table implementation

--- src/components/client/history/sales/Content.js
+++ src/components/client/history/sales/Content.js
@@ -1,27 +1,74 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import Typography from '@material-ui/core/Typography';
 import Paper from '@material-ui/core/Paper';
-import { withStyles } from '@material-ui/core/styles';
+import {withStyles} from '@material-ui/core/styles';
 import ContentStyle from "../../../common/ContentStyle";
 import {Helmet} from "react-helmet";
+import ReserveTable from "../../order/ReserveTable";
+import {refreshPeriod} from "../../../../util/RefreshPeriod";
+import {getOrders, getReserves} from "../../../../redux/actions/clientActions";
+import {connect} from "react-redux";
 
 const styles = theme => ContentStyle(theme);
 
-function Content({stat, getClientStatistic, setStatisticPeriod, ...props}) {
-    const {classes} = props;
+function Content({client, calls, getReserves, classes, ...props}) {
+    useEffect(() => {
+        if (!client.isReservesLoaded || (new Date() - client.reserveLoadingTime > refreshPeriod)) {
+            getReserves();
+        }
+    }, [client.isReservesLoaded, client.reserveLoadingTime, getReserves]);
+
+    const isReserveTablesShown = client && client.reserves;
 
     return (
         <Paper className={classes.paper}>
             <Helmet>
                 <title>Fenix - Історія відвантажень</title>
             </Helmet>
+
             <div className={classes.contentWrapper}>
-                <Typography color="textSecondary" align="center">
-                    Історія відвантажень
-                </Typography>
+                {isReserveTablesShown ?
+                    <div>
+                        {isReserveTablesShown && <ReserveTable
+                            reserves={client.reserves.map(el => {
+                                return {
+                                    id: el.id,
+                                    brand: el.brand,
+                                    number: el.number,
+                                    quantity: el.quantity,
+                                    description: el.description,
+                                    note: el.note,
+                                    orderDate: el.orderDate,
+                                    price: client.isEuroClient ? el.euro : el.uah,
+                                    euro: el.euro,
+                                    uah: el.uah,
+                                    date: el.date,
+                                    source: el.source
+                                }
+                            })} isEuroClient={client.isEuroClient}
+                        />}
+                    </div>
+                    :
+                    <Typography color="textSecondary" align="center">
+                        Інформація відсутня
+                    </Typography>
+                }
             </div>
         </Paper>
     );
 }
 
-export default withStyles(styles)(Content);
+// noinspection JSUnusedGlobalSymbols
+const mapDispatchToProps = {
+    getOrders,
+    getReserves
+};
+
+function mapStateToProps(state) {
+    return {
+        client: state.client,
+        calls: state.apiCallsInProgress
+    }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(withStyles(styles)(Content));
