9bd9b3d9e2db8ce0716f0723b7d842acc2e8e87a
Vasyl Suddya <vasya.suddya@gmail.com>
2021-04-11 08:46:58 +0300

    fix history stored procedures

--- functions/integration/sp_web_getreturnhistory.sql
+++ functions/integration/sp_web_getreturnhistory.sql
@@ -1,4 +1,4 @@
-CREATE PROCEDURE [dbo].[sp_web_getreturnhistory] @clientId INT, @page INT, @rowsPerPage INT
+CREATE PROCEDURE [dbo].[sp_web_getreturnhistory] @vip VARCHAR(10), @offset INT, @rows INT
 AS
 BEGIN
     SELECT dbo.[Подчиненная накладные].ID             AS id,
@@ -10,19 +10,21 @@ BEGIN
            dbo.Брэнды.Брэнд                           AS brand,
            dbo.[Каталог запчастей].[Номер поставщика] AS number,
            dbo.[Каталог запчастей].Описание           AS description,
-           COUNT(*) OVER ()                           as totalCount
+           COUNT(*) OVER ()                           AS totalCount
     FROM dbo.[Подчиненная накладные]
              INNER JOIN
          dbo.[Каталог запчастей] ON dbo.[Подчиненная накладные].ID_Запчасти = dbo.[Каталог запчастей].ID_Запчасти
              INNER JOIN
          dbo.Брэнды ON dbo.[Каталог запчастей].ID_Брэнда = dbo.Брэнды.ID_Брэнда
+             INNER JOIN
+         dbo.Клиенты ON dbo.[Подчиненная накладные].ID_Клиента = dbo.Клиенты.ID_Клиента
     WHERE (dbo.[Подчиненная накладные].ID_Накладной IS NOT NULL)
       AND (dbo.[Подчиненная накладные].Дата_закрытия IS NOT NULL)
-      AND (dbo.[Подчиненная накладные].ID_Клиента = @clientId)
+      AND (dbo.Клиенты.VIP LIKE @vip)
       AND (dbo.[Подчиненная накладные].Нету = 0)
       AND (dbo.[Подчиненная накладные].Обработано = 1)
       AND (dbo.[Подчиненная накладные].Количество < 0)
-    ORDER BY dbo.[Подчиненная накладные].ID DESC OFFSET @page * @rowsPerPage ROWS FETCH NEXT @rowsPerPage ROWS ONLY
+    ORDER BY dbo.[Подчиненная накладные].ID DESC OFFSET @offset ROWS FETCH NEXT @rows ROWS ONLY
 
 END
 go
