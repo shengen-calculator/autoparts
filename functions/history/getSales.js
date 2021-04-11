421c747df47822676096d4061ab3bbe93dd28319
Vasyl Suddya <vasya.suddya@gmail.com>
2021-04-11 09:27:54 +0300

    introduce history endpoints

---
+++ functions/history/getSales.js
@@ -0,0 +1,32 @@
+const functions = require('firebase-functions');
+const util = require('../util');
+const sql = require('mssql');
+const config = require('../mssql.connection').config;
+
+const getSales = async (data, context) => {
+
+    if (data && data.vip) {
+        util.checkForManagerRole(context);
+    } else {
+        util.checkForClientRole(context);
+    }
+
+    try {
+        const pool = await sql.connect(config);
+
+        const result = await pool.request()
+            .input('vip', sql.VarChar(10), data.vip ? data.vip : context.auth.token.vip)
+            .input('offset', sql.Int, data.offset ? data.offset : 0)
+            .input('rows', sql.Int, (data.rows && data.rows < 100) ? data.rows : 10)
+            .execute('sp_web_getsalehistory');
+        return result.recordset;
+    } catch (err) {
+        if (err) {
+            throw new functions.https.HttpsError('internal',
+                err.message);
+        }
+        return {err: err.message};
+    }
+};
+
+module.exports = getSales;
--- functions/index.js
+++ functions/index.js
@@ -6,5 +6,6 @@ exports.order = require('./orderFunc');
 exports.search = require('./searchFunc');
 exports.main = require('./mainFunc');
 exports.statistic = require('./statisticFunc');
+exports.history = require('./historyFunc');
 
 
