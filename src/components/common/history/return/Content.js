0693d7041229e9106dddb268999267c8a0f3add6
Vasyl Suddya <vasya.suddya@gmail.com>
2021-05-01 16:19:34 +0300

    history return table implementation

--- src/components/common/history/return/Content.js
+++ src/components/common/history/return/Content.js
@@ -4,24 +4,62 @@ import Paper from '@material-ui/core/Paper';
 import { withStyles } from '@material-ui/core/styles';
 import ContentStyle from "../../ContentStyle";
 import {Helmet} from "react-helmet";
+import ReturnTable from "../return/ReturnTable";
+import {getReturnHistory} from "../../../../redux/actions/historyActions";
+import {connect} from "react-redux";
 
 const styles = theme => ContentStyle(theme);
 
-function Content({stat, getClientStatistic, setStatisticPeriod, ...props}) {
-    const {classes} = props;
+function Content({client, getReturnHistory, classes}) {
+
+    const isTablesShown = client && client.returnHistory;
 
     return (
         <Paper className={classes.paper}>
             <Helmet>
                 <title>Fenix - Історія повернень</title>
             </Helmet>
+
             <div className={classes.contentWrapper}>
-                <Typography color="textSecondary" align="center">
-                    Історія повернень
-                </Typography>
+                {isTablesShown ?
+                    <div>
+                        {isTablesShown && <ReturnTable
+                            getReturnHistory={getReturnHistory}
+                            rowLoadingTime={client.returnHistoryLoadingTime}
+                            rowsTotal={client.returnHistory[0] ? client.returnHistory[0]['totalCount'] : 0}
+                            returns={client.returnHistory.map(el => {
+                                return {
+                                    id: el.id,
+                                    brand: el.brand,
+                                    number: el.number,
+                                    quantity: el.quantity,
+                                    description: el.description,
+                                    invoiceNumber: el.invoiceNumber,
+                                    invoiceDate: el.invoiceDate,
+                                    price: client.isEuroClient ? el['priceEur'] : el['priceUah']
+                                }
+                            })} isEuroClient={client.isEuroClient}
+                        />}
+                    </div>
+                    :
+                    <Typography color="textSecondary" align="center">
+                        Інформація відсутня
+                    </Typography>
+                }
             </div>
         </Paper>
     );
 }
 
-export default withStyles(styles)(Content);
+const mapDispatchToProps = {
+    getReturnHistory
+};
+
+function mapStateToProps(state) {
+    return {
+        client: state.client,
+        calls: state.apiCallsInProgress
+    }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(withStyles(styles)(Content));
---
+++ src/components/common/history/return/ReturnTable.js
@@ -0,0 +1,62 @@
+import React from 'react';
+import TableCell from '@material-ui/core/TableCell';
+import TableRow from '@material-ui/core/TableRow';
+import EnhancedTable from '../../EnhancedTable';
+
+
+const headCells = [
+    {id: 'invoiceNumber', numeric: false, disablePadding: false, label: 'Накладна №'},
+    {id: 'invoiceDate', numeric: false, disablePadding: false, label: 'Дата'},
+    {id: 'brand', numeric: false, disablePadding: false, label: 'Бренд'},
+    {id: 'number', numeric: false, disablePadding: false, label: 'Номер'},
+    {id: 'description', numeric: false, disablePadding: false, label: 'Опис'},
+    {id: 'quantity', numeric: true, disablePadding: false, label: 'К-сть'},
+    {id: 'price', numeric: true, disablePadding: false, label: 'Ціна'},
+];
+
+function tableRow(row, index, isSelected) {
+    const isItemSelected = isSelected(row.id);
+    return (
+        <TableRow
+            hover
+            role="checkbox"
+            aria-checked={isItemSelected}
+            tabIndex={-1}
+            key={row.id}
+            selected={isItemSelected}
+        >
+            <TableCell align="left">{row.invoiceNumber}</TableCell>
+            <TableCell align="left">{row.invoiceDate}</TableCell>
+            <TableCell align="left">{row.brand}</TableCell>
+            <TableCell align="left">{row.number}</TableCell>
+            <TableCell align="left">{row.description}</TableCell>
+            <TableCell align="right">{row.quantity}</TableCell>
+            <TableCell align="right">{row.price.toFixed(2)}</TableCell>
+        </TableRow>
+    );
+}
+
+export default function ReturnTable(props) {
+
+    return (
+        <React.Fragment>
+
+            <EnhancedTable
+                rows={props.returns}
+                getRowsFunc={props.getReturnHistory}
+                rowLoadingTime={props.rowLoadingTime}
+                isMinimalTableHeadUse={true}
+                headCells={headCells}
+                tableRow={tableRow}
+                rowsTotal={props.rowsTotal}
+                title="Повернення"
+                columns={7}
+                isFilterShown={false}
+                rowsPerPageOptions={[20, 50, 100]}
+                isRowSelectorShown={false}
+                noRecordsMessage="Повернення відсутні"
+            />
+
+        </React.Fragment>
+    );
+}
