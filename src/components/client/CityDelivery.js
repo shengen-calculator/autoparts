ea62490b79d40e8234d5004ca0c830acec74e36d
Vasyl Suddya <vasya.suddya@gmail.com>
2021-09-22 10:45:40 +0300

    implement logic

--- src/components/client/CityDelivery.js
+++ src/components/client/CityDelivery.js
@@ -1,13 +1,18 @@
 import Typography from "@material-ui/core/Typography";
-import React from "react";
+import React, {useState} from "react";
 import IconButton from "@material-ui/core/IconButton";
 import AirportShuttleIcon from "@material-ui/icons/AirportShuttle";
 import {withStyles} from "@material-ui/core/styles";
 import Tooltip from "@material-ui/core/Tooltip";
+import {getDay, startOfToday, addMinutes, format} from 'date-fns'
 
 function CityDelivery() {
     const bolder = {fontWeight: 800};
     const bigger = {fontWeight: 800, fontSize: 32};
+
+    const [timer, setTimer] = useState({
+        isShown: false
+    });
 
     const HtmlTooltip = withStyles((theme) => ({
         tooltip: {
@@ -16,24 +21,65 @@ function CityDelivery() {
             maxWidth: 340,
             fontSize: theme.typography.pxToRem(12),
             border: '1px solid #dadde9',
+
         },
     }))(Tooltip);
 
+    const formattedTime = (minutes) => {
+        const helperDate = addMinutes(new Date(0), minutes - 180);
+        return format(helperDate, 'hh:mm');
+    };
+
+    const handleClick = () => {
+        const date = new Date();
+        //0 represents Sunday
+        let text = "";
+        const current = (date - startOfToday()) / 1000 / 60;
+        let dispatchTime;
+        if ([1, 2, 3, 4, 5].includes(getDay(date))) {
+            dispatchTime = [630, 750, 870, 990, 1110];
+        } else if ([6].includes(getDay(date))) {
+            dispatchTime = [630, 750, 870];
+        } else {
+            dispatchTime = [];
+        }
+        for (const time of dispatchTime) {
+            if (current < time) {
+                text = formattedTime(time - current);
+                break;
+            }
+        }
+        if (!text) {
+            text = "-";
+        }
+
+        setTimer(prev => ({
+            ...prev,
+            isShown: !prev.isShown,
+            text: text
+        }));
+
+    };
+
     return (
-        <HtmlTooltip title={
-            <React.Fragment>
-                До наступного виїзду авто залишилось:
-                <Typography color="inherit" style={bigger}>00.01.25</Typography>
-                Графік відправлень:
-                <Typography color="inherit" style={bolder}>ПНД - ПТ: 10.30 12.30 14.30 16.30 18.30</Typography>
-                <Typography color="inherit" style={bolder}>СБ: 10.30 12.30 14.30</Typography>
-            </React.Fragment>
-        }>
+        <HtmlTooltip
+            open={timer.isShown}
+            disableFocusListener
+            disableHoverListener
+            disableTouchListener
+            title={
+                <React.Fragment>
+                    До наступного виїзду авто залишилось:
+                    <Typography color="inherit" style={bigger}>{timer.text}</Typography>
+                    Графік виїздів:
+                    <Typography color="inherit" style={bolder}>Пон-Пят: 10.30 12.30 14.30 16.30 18.30</Typography>
+                    <Typography color="inherit" style={bolder}>Суб: 10.30 12.30 14.30</Typography>
+                </React.Fragment>
+            }>
             <IconButton color="inherit">
-                <AirportShuttleIcon/>
+                <AirportShuttleIcon onClick={handleClick}/>
             </IconButton>
         </HtmlTooltip>
-
     )
 }
 
