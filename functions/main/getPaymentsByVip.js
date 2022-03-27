cf47df15cbc6fd7cefba21cc8d0f18b712e76cc5
Vasyl Suddya <vasya.suddya@gmail.com>
2022-03-27 16:06:03 +0200

    move sql query to cloud func getPaymentsByVip

--- functions/main/getPaymentsByVip.js
+++ functions/main/getPaymentsByVip.js
@@ -12,11 +12,35 @@ const getPaymentsByVip = async (data, context) => {
     }
 
     try {
-        const pool = await sql.connect(config);
+        await sql.connect(config);
+        const clientQuery = `                
+                SELECT TOP (1) ID_Клиента as id
+                              ,ISNULL([Количество_дней], 0) as days
+                FROM [dbo].[Клиенты]
+                WHERE VIP like '${data ? data : context.auth.token.vip}';
+        `;
+        const client = await sql.query(clientQuery);
+        const id = parseInt(client.recordset[0]['id']);
+        const days = parseInt(client.recordset[0]['days']);
 
-        const result = await pool.request()
-            .input('vip', sql.VarChar(10), data ? data : context.auth.token.vip)
-            .execute('sp_web_getpaymentplanbyvip');
+        const paymentQuery = `
+                    WITH gen AS (
+                        SELECT DATEADD(day, 0, CURRENT_TIMESTAMP) AS PaymentDate,
+                               dbo.GetAmountOverdueDebt(${id}, ${-days}) - 0 AS Amount,
+                               0 AS num
+                        UNION ALL
+                        SELECT DATEADD(day, num + 1, CURRENT_TIMESTAMP)                                                        as PaymentDate,
+                               (dbo.GetAmountOverdueDebt(${id}, ${-days} + num + 1) -
+                                dbo.GetAmountOverdueDebt(${id}, ${-days} + num))                                                   as Amount,
+                               num + 1
+                        FROM gen
+                        WHERE num + 1 <= ${days}
+                    )
+                    SELECT FORMAT(PaymentDate, 'd', 'de-de') as date, Amount as amount
+                    FROM gen
+        `;
+
+        const result = await sql.query(paymentQuery);
         return result.recordset;
     } catch (err) {
         if(err) {
