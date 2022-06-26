3997b230300e8dd0ec340437618766bc3fc3fced
Vasyl Suddya <vasya.suddya@gmail.com>
2022-06-26 18:26:40 +0300

    update get delivery date func, use partId instead of warehouse id

--- functions/search/getDeliveryDate.js
+++ functions/search/getDeliveryDate.js
@@ -7,17 +7,21 @@ const getDeliveryDate = async (data, context) => {
 
     util.checkForClientRole(context);
 
-    if (!data || typeof data.warehouseId === 'undefined' || typeof data.term === 'undefined') {
+    if (!data || typeof data.partId === 'undefined' || typeof data.term === 'undefined') {
         throw new functions.https.HttpsError('invalid-argument',
-            'The function must be called with the next arguments "warehouseId, term"');
+            'The function must be called with the next arguments "partId, term"');
     }
 
     try {
         await sql.connect(config);
         const query =`
-            SELECT dbo.GetArrivalDate(${data.warehouseId}, ${data.term}) as ArrivalDate, ArrivalTime 
+            SELECT dbo.GetArrivalDate((
+                SELECT WarehouseId FROM [Каталоги поставщиков] WHERE ID_Запчасти = ${data.partId}
+            ), ${data.term}) as ArrivalDate, ArrivalTime 
             FROM SupplierWarehouse 
-            WHERE Id = ${data.warehouseId}
+            WHERE Id = (
+                SELECT WarehouseId FROM [Каталоги поставщиков] WHERE ID_Запчасти = ${data.partId}
+            )
         `;
 
         const result = await sql.query(query);
