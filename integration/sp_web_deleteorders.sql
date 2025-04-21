be6b129a070001ebfba05498b31fc960eb2c08c4
Shengen <35036421+shengen-calculator@users.noreply.github.com>
2025-04-21 10:19:56 +0300

    remove sql-files (#99)

--- integration/sp_web_deleteorders.sql
+++
@@ -1,7 +0,0 @@
-CREATE PROCEDURE [dbo].[sp_web_deleteorders]
-    @ids varchar(300)
-AS
-BEGIN
-	UPDATE dbo.[Запросы клиентов] SET Заказано = 0, Обработано = 1 WHERE ID_Запроса IN (SELECT Name FROM dbo.SplitString (@ids))
-END
-go
